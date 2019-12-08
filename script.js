4166b92405b9c7ba6d25b70eee18e14bbaf1262e
Richard Gobert <dev.richard.g@gmail.com>
2019-12-08 18:03:28 +0100

    modification event click and close aniamte in one function checkAndAnimateItems, add checkAndAnimateItem and indexToCheckAndAnimate a function that opens and closes the click image as well as the buttons for their index

--- script.js
+++ script.js
@@ -8,38 +8,31 @@ $(function () {
         init = function () {
             bindEvents();
             if (validIndex(openedIdenx))
-                animateItem($mainMenuItems.eq(openedIdenx), true, 700)
+                animateItem($mainMenuItems.eq(openedIdenx), true, 700);
 
         },
 
         bindEvents = function () {
             $mainMenuItems.children(".pictures").click(function () {
-                let newIndex = $(this).parent().index(),
-                    $item = $mainMenuItems.eq(newIndex);
-
-                if (openedIdenx === newIndex) {
-                    animateItem($item, false, 250);
-                    openedIdenx = -1;
-                }
-                else {
-                    if (validIndex(newIndex)) {
-                        animateItem($mainMenuItems.eq(openedIdenx), false, 250);
-                        openedIdenx = newIndex;
-                        animateItem($item, true, 250);
-                    }
-                }
+                let newIndex = $(this).parent().index();
+                checkAndAnimateItem(newIndex);
             });
+
             $(".button").hover(function () {
                 $(this).addClass("hovered");
             },
                 function () {
                     $(this).removeClass("hovered");
                 });
+
+            $(".button").click(function () {
+                let newIndex = $(this).index();
+                checkAndAnimateItem(newIndex);
+            });
         },
 
-
         validIndex = function (indexToCheck) {
-            return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
+            return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
         },
 
         animateItem = function ($item, toOpen, speed) {
@@ -49,6 +42,20 @@ $(function () {
 
             $colorImage.animate($colorImageParam, speed);
             $item.animate(itemParam, speed);
+        },
+
+        checkAndAnimateItem = function (indexToCheckAndAnimate) {
+            if (openedIdenx === indexToCheckAndAnimate) {
+                animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
+                openedIdenx = -1;
+            }
+            else {
+                if (validIndex(indexToCheckAndAnimate)) {
+                    animateItem($mainMenuItems.eq(openedIdenx), false, 250);
+                    openedIdenx = indexToCheckAndAnimate;
+                    animateItem($mainMenuItems.eq(openedIdenx), true, 250);
+                }
+            }
         };
     init();
 });
