747e15d757d3ec724e70098cc7535fa6c6037561
Richard Gobert <dev.richard.g@gmail.com>
2019-12-08 15:07:11 +0100

    modification function init open and close, add function teste index and add variable itemparam and colorparam into js, modification function animate position actresses

--- script.js
+++ script.js
@@ -8,14 +8,33 @@ $(function () {
         init = function () {
             $mainMenuItems.children(".pictures").click(function () {
                 let newIndex = $(this).parent().index(),
-                    $item = $mainMenuItems.eq(newIndex),
-                    $colorImage = $item.find(".color");
+                    $item = $mainMenuItems.eq(newIndex);
 
-                $colorImage.animate({ left: "0" }, 250);
-                $item.animate({ width: "42rem" }, 250);
-
-                openedIdenx = newIndex;
+                if (openedIdenx === newIndex) {
+                    animateItem($item, false, 250);
+                    openedIdenx = -1;
+                }
+                else {
+                    if (validIndex(newIndex)) {
+                        animateItem($mainMenuItems.eq(openedIdenx), false, 250);
+                        openedIdenx = newIndex;
+                        animateItem($item, true, 250);
+                    }
+                }
             });
+        },
+
+        validIndex = function (indexToCheck) {
+            return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems)
+        },
+
+        animateItem = function ($item, toOpen, speed) {
+            let $colorImage = $item.find(".color"),
+                itemParam = toOpen ? { width: "42rem" } : { width: "14rem" },
+                $colorImageParam = toOpen ? { left: "0" } : { left: "14rem" };
+
+            $colorImage.animate($colorImageParam, speed);
+            $item.animate(itemParam, speed);
         };
     init();
 });
