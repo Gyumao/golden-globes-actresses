5dc65ec85ca4b54bdc917af3fcd4da502963b619
Richard Gobert <dev.richard.g@gmail.com>
2019-12-08 14:00:14 +0100

    modification inport jquery, add function animate image color on click and reminder funcition init

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+"use strict";
+
 $(function () {
     let $mainMenuItems = $("#main-menu ul").children("li"),
         totalMainMenuItems = $mainMenuItems.length,
         openedIdenx = -1,
 
         init = function () {
-            $mainMenuItems.children(".images").click(function () {
+            $mainMenuItems.children(".pictures").click(function () {
                 let newIndex = $(this).parent().index(),
                     $item = $mainMenuItems.eq(newIndex),
                     $colorImage = $item.find(".color");
+
+                $colorImage.animate({ left: "0" }, 250);
+                $item.animate({ width: "42rem" }, 250);
+
+                openedIdenx = newIndex;
             });
         };
+    init();
 });
